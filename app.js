fe5904af318934f78c63eb3b1fe3efe14233a2e0
Julien Marcenac <julien.marcenac2@gmail.com>
2023-01-16 17:51:14 +0100

    post done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 require("dotenv").config();
 
 const port = process.env.APP_PORT ?? 5000;
@@ -14,13 +16,21 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 
+// get movies
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
+// post movies
+app.post("/api/movies", movieHandlers.postMovie);
+
 const userHandlers = require("./userHandlers");
 
+// get users
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUsersById);
+
+// post movies
+app.post("/api/users", userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
--- database.js
+++ database.js
@@ -19,14 +19,14 @@ const database = mysql.createPool({
     console.error(err);
   });
 
-  database
-  .query("select * from movies")
-  .then((result) => {
-    const movies = result[0];
-    console.log(movies);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+  // database
+  // .query("select * from movies")
+  // .then((result) => {
+  //   const movies = result[0];
+  //   console.log(movies);
+  // })
+  // .catch((err) => {
+  //   console.error(err);
+  // });
 
   module.exports = database;
