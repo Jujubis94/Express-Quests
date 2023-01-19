384283c2efd0beb6879260a5ce7d27a16000bd3f
Jujubis94 <113546537+Jujubis94@users.noreply.github.com>
2023-01-19 08:57:03 +0100

    Update userHandlers.js

--- userHandlers.js
+++ userHandlers.js
@@ -84,27 +84,6 @@ const getUsers = (req, res) => {
       });
   };
 
-  const putMovie = (req, res) => {
-    const id = Number(req.params.id);
-    const { title, director, year, color, duration } = req.body;
-    database
-      .query(
-        "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
-        [title, director, year, color, duration, id]
-      )
-      .then(([result]) => {
-        if (result.affectedRows === 0) {
-          res.status(404).send("Not Found");
-        } else {
-          res.sendStatus(204);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error editing the movie");
-      });
-  };
-
   const deleteUser = (req, res) => {
     const id = Number(req.params.id);
     database
