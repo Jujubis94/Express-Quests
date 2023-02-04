b0b8028740105cf72406b39f88faef2ca8477b3d
Julien Marcenac <julien.marcenac2@gmail.com>
2023-02-04 15:33:59 +0100

    quest 08 done

--- app.js
+++ app.js
@@ -9,19 +9,33 @@ require("dotenv").config();
 
 const port = process.env.APP_PORT ?? 5000;
 
-const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
-};
+const userHandlers = require("./userHandlers");
 
-app.get("/", welcome);
+// Password
+const { hashPassword, verifyPassword, testIdSub } = require("./auth.js");
 
 // Password
 const {verifyToken } = require("./auth.js");
 const movieHandlers = require("./movieHandlers");
 
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+// public
+app.get("/", welcome);
 // get movies
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+// get users
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUsersById);
+// user login
+app.post("/api/login", userHandlers.getUserByEmailWithPasswordAndPassToNext, verifyPassword)
+// post users
+app.post("/api/users", validateUser, hashPassword, userHandlers.postUser);
+
+app.use(verifyToken)
 
 // post movies
 app.post("/api/movies",validateMovie, verifyToken, movieHandlers.postMovie);
@@ -32,25 +46,11 @@ app.put("/api/movies/:id",validateMovie, movieHandlers.putMovie)
 // delete movies
 app.delete("/api/movies/:id", movieHandlers.deleteMovie)
 
-const userHandlers = require("./userHandlers");
-
-// Password
-const { hashPassword, verifyPassword } = require("./auth.js");
-
-// user login
-app.post("/api/login", userHandlers.getUserByEmailWithPasswordAndPassToNext, verifyPassword)
-// get users
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUsersById);
-
-// post users
-app.post("/api/users", validateUser, hashPassword, userHandlers.postUser);
-
 // put users
-app.put("/api/users/:id", validateUser, hashPassword, userHandlers.putUser)
+app.put("/api/users/:id", testIdSub, validateUser, hashPassword, userHandlers.putUser)
 
 // delete users
-app.delete("/api/users/:id", userHandlers.deleteUser)
+app.delete("/api/users/:id", testIdSub, userHandlers.deleteUser)
 
 app.listen(port, (err) => {
   if (err) {
--- auth.js
+++ auth.js
@@ -13,8 +13,8 @@ const hashPassword = (req, res, next) => {
     // recupere le password avec les options
     .hash(req.body.password, hashingOptions)
     .then((hashedPassword) => {
-    //   console.log(hashedPassword);
-    //   ajoute un hashPassword a l'objet requete
+      //   console.log(hashedPassword);
+      //   ajoute un hashPassword a l'objet requete
       req.body.hashedPassword = hashedPassword;
       // supprime le password en clair
       delete req.body.password;
@@ -28,48 +28,58 @@ const hashPassword = (req, res, next) => {
 };
 
 const verifyPassword = (req, res) => {
-    argon2
+  argon2
     .verify(req.user.hashedPassword, req.body.password)
     .then((verifiedPassword) => {
-        if (verifiedPassword) {
-            const payload = {sub: req.user.id}
+      if (verifiedPassword) {
+        const payload = { sub: req.user.id };
 
-            const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "1h"});
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
 
-          res.send({ token, user: req.user });
-        } else {
-          res.sendStatus(401);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        res.sendStatus(500);
-      });
-  };
-
-  const verifyToken = (req, res, next) => {
-    try {
-        const authorizationHeader = req.header("Authorization");
-        if (authorizationHeader == null) {
-            throw new Error("Authorization header is missing");
-        }
-        const [type, token] = authorizationHeader.split(" ");
-
-        if (type !== "Bearer") {
-          throw new Error("Authorization header has not the 'Bearer' type");
-        }
-    
-        req.payload = jwt.verify(token, process.env.JWT_SECRET);
-    
-        next();
-      } catch (err) {
-        console.error(err);
+        res.send({ token, user: req.user });
+      } else {
         res.sendStatus(401);
       }
-    };
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
 
+const verifyToken = (req, res, next) => {
+  try {
+    const authorizationHeader = req.header("Authorization");
+    if (authorizationHeader == null) {
+      throw new Error("Authorization header is missing");
+    }
+    const [type, token] = authorizationHeader.split(" ");
+
+    if (type !== "Bearer") {
+      throw new Error("Authorization header has not the 'Bearer' type");
+    }
+
+    req.payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    next();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+};
+
+const testIdSub = (req, res, next) => {
+  if (Number(req.params.id) === req.payload.sub) {
+    next();
+  }
+  console.log(req.payload.sub, Number(req.params.id));
+  res.status(403).send("Forbidden");
+};
 module.exports = {
   hashPassword,
   verifyPassword,
-  verifyToken
+  verifyToken,
+  testIdSub,
 };
